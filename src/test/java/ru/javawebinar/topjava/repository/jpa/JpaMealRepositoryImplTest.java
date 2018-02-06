adf8076f4b1a49795a3fde37e0245faaf2f67ff3
AZAZA <exotika37@gmail.com>
2018-02-06 11:23:18 +0300

    HW4

---
+++ src/test/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImplTest.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.repository.jpa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JpaMealRepositoryImplTest {
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void getBetween() throws Exception {
+    }
+
+}
